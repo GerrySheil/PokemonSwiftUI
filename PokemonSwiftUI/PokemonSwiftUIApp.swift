25beec7e545cbbbe4f016c34d56644a5dfbfbb8b
Gerry Sheil <gerrysheil01@gmail.com>
2024-05-09 10:31:27 +0100

    Initial Commit

---
+++ PokemonSwiftUI/PokemonSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonSwiftUIApp.swift
+//  PokemonSwiftUI
+//
+//  Created by Gerry Sheil on 09/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokemonSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
